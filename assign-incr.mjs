303e565195714a8e8632e07414ebf6fbbf644481
Pau Fernández <pau.fernandez@upc.edu>
2026-01-28 18:05:44 +0100

    Assignaciones con aritmética

---
+++ assign-incr.mjs
@@ -0,0 +1,8 @@
+
+let variableConNombreMuyLargo = 123;
+let diferencia = 5;
+
+variableConNombreMuyLargo += diferencia; // sumar lo que hay a la derecha
+variableConNombreMuyLargo *= 2;
+variableConNombreMuyLargo /= 5;
+variableConNombreMuyLargo -= 1;

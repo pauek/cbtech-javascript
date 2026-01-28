b9ba09f754121399201f69b245023c91123a2ae4
Pau Fernández <pau.fernandez@upc.edu>
2026-01-28 18:37:35 +0100

    Ejemplo ternaria

---
+++ ternaria.mjs
@@ -0,0 +1,11 @@
+let a = -3
+
+let resultado
+if (a >= 0.0) {
+    resultado = "positivo"
+} else {
+    resultado = "negativo"
+}
+
+console.log(resultado)
+console.log(a >= 0.0 ? "positivo" : "negativo")
